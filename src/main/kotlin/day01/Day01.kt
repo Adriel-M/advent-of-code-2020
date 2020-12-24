ac55c03f9bf39954253d97431b3b94e175343f4e
Adriel Martinez <adriel@adrielm.ca>
2020-12-24 17:39:26 -0500

    move to algorithms class

--- src/main/kotlin/day01/Day01.kt
+++ src/main/kotlin/day01/Day01.kt
@@ -1,29 +1,24 @@
 package day01
 
+import common.Algorithms
 import common.Solution
 import org.koin.core.KoinComponent
-import org.koin.core.inject
 import org.koin.dsl.module
 
 class Day01 : Solution<List<Int>, Int>(1), KoinComponent {
-    private val twoSum: TwoSum by inject()
-    private val threeSum: ThreeSum by inject()
-
     override fun solveProblem1(input: List<Int>): Int {
-        val (a, b) = twoSum.find(input, 2020)!!
+        val (a, b) = Algorithms.twoSum(input, 2020)!!
         return a * b
     }
 
     override fun solveProblem2(input: List<Int>): Int {
-        val (a, b, c) = threeSum.find(input, 2020)!!
+        val (a, b, c) = Algorithms.threeSum(input, 2020)!!
         return a * b * c
     }
 
     companion object {
         val module = module {
             single { Day01() }
-            single { TwoSum() }
-            single { ThreeSum() }
         }
     }
 }
